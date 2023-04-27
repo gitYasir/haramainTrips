0241f1fdc0d250470cb6958255c844fd9e1ccb55
Yasir <yasir.ibrahim@hotmail.co.uk>
2023-04-27 06:54:03 +0100

    added google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,24 @@
-import '@/styles/globals.css'
+import Script from "next/script";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-LR8SQMNH61"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-LR8SQMNH61');
+    `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
